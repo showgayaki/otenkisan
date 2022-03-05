4dee9c1cda7f266a041ac71cc966e1f281e178d4
showgayaki <knk.lee.kj@gmail.com>
2022-03-06 03:57:28 +0900

    日付・時計部分作成

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <DatetimeNow />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import '../src/assets/css/reset.css'
+import DatetimeNow from './components/DatetimeNow.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        DatetimeNow,
+    }
+});
+</script>
+
+<style>
+#app{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #fff;
+}
+html{
+    background: #000;
+}
+.container{
+    padding: 60px 30px 50px;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/DatetimeNow.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="datetime-now">
+        <h1 class="datetime-now__date">{{ currentDate }}</h1>
+        <p class="datetime-now__clock">
+            <span v-for="time in currentTime" :key="time" class="datetime-now__time">{{ time }}</span>
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'DatetimeNow',
+    data() {
+        return{
+            currentDate: '',
+            currentTime: [] as string[],
+            week: ['(日)', '(月)', '(火)', '(水)', '(木)',  '(金)', '(土)'],
+        }
+    },
+    mounted: function(){
+        let timerId = setInterval(this.getDatetimeNow, 1000);
+    },
+    methods: {
+        getDatetimeNow(){
+            const now: Date = new Date;
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
+
+            this.currentDate = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + this.week[now.getDay()];
+            this.currentTime = [hours, minutes, seconds]
+        }
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.datetime-now{
+    &__date{
+        margin-bottom: 30px;
+        font-size: 65px;
+    }
+    &__clock{
+        font-size: 120px;
+        text-align: center;
+    }
+    &__time{
+        color: #fff;
+        &:not(:last-child)::after{
+            content: ":";
+            display: inline-block;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        &__date{
+            margin-bottom: 30px;
+            font-size: 24px;
+        }
+        &__clock{
+            font-size: 60px;
+        }
+    }
+}
+</style>
